1df58075fa2b754517ce2b1d2fe92346b6567138
vighnesh11 <kargutkar860@gmail.com>
2024-10-17 23:45:26 +0530

    slider added

--- vite-project/src/components/MainPages/Home.jsx
+++ vite-project/src/components/MainPages/Home.jsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { Helmet } from "react-helmet";
+import "./Home.css";
 
 export default function HomePage() {
   return (
@@ -7,8 +8,40 @@ export default function HomePage() {
       <Helmet>
         <title>Home</title>
       </Helmet>
-      <h1>Home Page</h1>
-      <h1>Wellcome</h1>
+      <div className="slider">
+        <div className="slide-track">
+          <div className="slide">
+            <h1>slide 1</h1>
+          </div>
+          <div className="slide">
+            <h1>slide 2</h1>
+          </div>
+          <div className="slide">
+            <h1>slide 3</h1>
+          </div>
+          <div className="slide">
+            <h1>slide 4</h1>
+          </div>
+          <div className="slide">
+            <h1>slide 5</h1>
+          </div>
+          <div className="slide">
+            <h1>slide 6</h1>
+          </div>
+          <div className="slide">
+            <h1>slide 7</h1>
+          </div>
+          <div className="slide">
+            <h1>slide 8</h1>
+          </div>
+          <div className="slide">
+            <h1>slide 9</h1>
+          </div>
+        </div>
+        {/* <h2>Consult Specialist Doctors Online</h2>
+        <h2>on Video/Audio Call & and Chat</h2> */}
+      </div>
+      <h1>Wellcome to RHSC</h1>
     </Fragment>
   );
 }
